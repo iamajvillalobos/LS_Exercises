e4a613a9fbe436f3457c527ca41f9e9b676ec69b
Arman Jon Villalobos <me@armanjon.com>
2016-06-30 22:39:30 +0800

    Prettify a little bit the inputs

--- rps/human.rb
+++ rps/human.rb
@@ -7,7 +7,7 @@ class Human < Player
   def set_name
     n = ""
     loop do
-      puts "What's your name?"
+      print "What's your name? "
       n = gets.chomp
       break unless n.empty?
       puts "Sorry, you must input a name."
@@ -20,7 +20,7 @@ class Human < Player
     choice = nil
 
     loop do
-      puts 'Choose a move (rock, paper, scissors, lizard, spock):'
+      print 'Choose a move (rock, paper, scissors, lizard, spock): '
       choice = gets.chomp
       break if Move::VALUES.include?(choice)
       puts "Sorry, you must input correct choice."
