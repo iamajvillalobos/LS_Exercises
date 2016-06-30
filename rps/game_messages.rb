b8586d7cd6de6a8c23c77282bf2ae8501a0c4389
Arman Jon Villalobos <me@armanjon.com>
2016-06-30 22:39:28 +0800

    Created GameMessages for displaying messages

---
+++ rps/game_messages.rb
@@ -0,0 +1,30 @@
+module GameMessages
+  GAME_OPTIONS = %w(rock paper scissors lizard spock).freeze
+
+  def welcome_message
+    puts "=> Welcome to #{game_title}"
+  end
+
+  def game_title
+    GAME_OPTIONS.map(&:capitalize).join(' ')
+  end
+
+  def goodbye_message
+    puts "=> Thanks for playing #{game_title}. Goodbye!"
+  end
+
+  def display_player_moves(player1, player2)
+    puts "=> #{player1.name} plays #{player1.move}"
+    puts "=> #{player2.name} plays #{player2.move}"
+  end
+
+  def display_winner_message(player)
+    puts "=> #{player.name} wins the round!"
+  end
+
+  def display_no_winner_message
+    puts "=> It's a tie!"
+  end
+end
+
+# 1899144011
